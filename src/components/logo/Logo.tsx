05b47466b2d1117483017b66ae05ec34075c3269
Deusnoire <andrewnzaikombe@gmail.com>
2023-03-29 21:16:02 +0300

    Styled logo

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -1,7 +1,7 @@
 import "./Logo.css";
 
 function Logo(){
-    return <div className="logo">
+    return <div className="logo hoverable">
         <img src="./assets/images/reddit.jpg" alt=""/>
         <span>reddit</span>
     </div>;
