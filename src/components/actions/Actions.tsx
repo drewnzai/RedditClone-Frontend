71be590a274b55b8ea75bc1a77aa1fb4df866aa5
Deusnoire <andrewnzaikombe@gmail.com>
2023-03-30 14:14:40 +0300

    Added Dropdown Menu

--- src/components/actions/Actions.tsx
+++ src/components/actions/Actions.tsx
@@ -1,17 +1,61 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+
 import "./Actions.css";
+import React, {useState, useEffect, useRef} from 'react';
 import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import Button from "../button/Button";
 
 function Actions(){
+
+    const [open, setOpen] = useState(false);
+
+
+  let menuRef = useRef<HTMLInputElement>(null);
+
+    useEffect(() => {
+        let handler = (e:any)=>{
+          if(!menuRef!.current!.contains(e.target)){
+            setOpen(false);
+            console.log(menuRef.current);
+          }      
+        };
+    
+        document.addEventListener("mousedown", handler);
+        
+    
+        return() =>{
+          document.removeEventListener("mousedown", handler);
+        }
+    
+      });
+    
+    
     return <div className="actions">
         <Button primary label="Get App"/>
         <Button label="Log In"/>
-        <div className="profile">
+        
+        <div className="profile" onClick={()=>{setOpen(!open)}}>
             <PersonOutlineOutlinedIcon/>
             <ArrowDropDownIcon/>
-            </div>
-    </div>;
+        </div>
+
+        <div className={`dropdown-menu ${open? 'active' : 'inactive'}`} ref={menuRef}>
+          <ul>
+            <DropdownItem  text = {"Log In/Sign Up"}/>
+          </ul>
+        </div>
+
+
+      </div>;
+}
+
+function DropdownItem(props:any){
+  return(
+    <li className = 'dropdownItem'>
+      <a> {props.text} </a>
+    </li>
+  );
 }
 
 export default Actions;
