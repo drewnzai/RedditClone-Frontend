34ff2c43bb54872cd4fdb8431dd7c016ed118b1c
Deusnoire <andrewnzaikombe@gmail.com>
2023-04-04 21:10:26 +0300

    Created ListItem component in Sidebar component

--- src/components/content/sidebar/Sidebar.tsx
+++ src/components/content/sidebar/Sidebar.tsx
@@ -4,40 +4,13 @@ import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 function Sidebar(){
     return <div>
         <div className='community'>
-            <div className='list-item'>
-                <span>POPULAR COMMUNITIES</span>
-                 <ExpandMoreIcon/>
-            </div>
-            
-            <div className='list-item'>
-                <span>POPULAR COMMUNITIES</span>
-                 <ExpandMoreIcon/>
-            </div>
-            
-            <div className='list-item'>
-                <span>POPULAR COMMUNITIES</span>
-                 <ExpandMoreIcon/>
-            </div>
-            
-            <div className='list-item'>
-                <span>POPULAR COMMUNITIES</span>
-                 <ExpandMoreIcon/>
-            </div>
-            
-            <div className='list-item'>
-                <span>POPULAR COMMUNITIES</span>
-                 <ExpandMoreIcon/>
-            </div>
-            
-            <div className='list-item'>
-                <span>POPULAR COMMUNITIES</span>
-                 <ExpandMoreIcon/>
-            </div>
-            
-            <div className='list-item'>
-                <span>POPULAR COMMUNITIES</span>
-                 <ExpandMoreIcon/>
-            </div>
+        <ListItem text='POPULAR COMMUNITIES'/>
+        <ListItem text='POPULAR COMMUNITIES'/>
+        <ListItem text='POPULAR COMMUNITIES'/>
+        <ListItem text='POPULAR COMMUNITIES'/>
+        <ListItem text='POPULAR COMMUNITIES'/>
+        <ListItem text='POPULAR COMMUNITIES'/>    
+        <ListItem text='POPULAR COMMUNITIES'/>
         </div>
         
         <div>
@@ -45,5 +18,14 @@ function Sidebar(){
         </div>
     </div>;
 }
+
+
+export function ListItem(props: any){
+    return <div className='list-item'>
+    <span>{props.text}</span>
+     <ExpandMoreIcon/>
+</div>
+}
+
 
 export default Sidebar;
