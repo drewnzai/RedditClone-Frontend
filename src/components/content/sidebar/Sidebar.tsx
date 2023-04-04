5394dc6a2c58f15afd9e960b22345c641aa823a7
Deusnoire <andrewnzaikombe@gmail.com>
2023-04-04 20:33:14 +0300

    Aligned sidebar correctly

--- src/components/content/sidebar/Sidebar.tsx
+++ src/components/content/sidebar/Sidebar.tsx
@@ -3,7 +3,19 @@ import './Sidebar.css';
 
 function Sidebar(){
     return <div>
-        Community        
+        <div className='community'>
+            <div>Item</div>
+            <div>Item</div>
+            <div>Item</div>
+            <div>Item</div>
+            <div>Item</div>
+            <div>Item</div>
+            <div>Item</div>
+        </div>
+        
+        <div>
+
+        </div>
     </div>;
 }
 
