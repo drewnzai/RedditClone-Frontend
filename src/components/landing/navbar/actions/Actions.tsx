e8f3524434bf9c2e39e22111c2de29e8757f27e1
Deusnoire <andrewnzaikombe@gmail.com>
2023-04-04 20:03:10 +0300

    Removed unnecessary import

--- src/components/landing/navbar/actions/Actions.tsx
+++ src/components/landing/navbar/actions/Actions.tsx
@@ -2,7 +2,6 @@
 
 import "./Actions.css";
 import React, {useState, useEffect, useRef} from 'react';
-import ReactDOM from 'react-dom';
 import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import QrCodeScannerSharpIcon from '@mui/icons-material/QrCodeScannerSharp';
