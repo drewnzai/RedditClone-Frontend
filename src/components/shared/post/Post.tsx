e545d2879b3c116e43967afe2a5094810871a3e1
Deusnoire <andrewnzaikombe@gmail.com>
2023-04-05 17:31:37 +0300

    Finished creating the post footer grid section

--- src/components/shared/post/Post.tsx
+++ src/components/shared/post/Post.tsx
@@ -10,6 +10,7 @@ function Post(){
         <div className='post-wrapper'>
            {Posts.map((post, index) => (
             <div className='post'>
+              
               <div className='post-sidebar'>
                 <ArrowUpward className='upvote'/>
                 <span>{post.upvotes}</span>
@@ -17,7 +18,7 @@ function Post(){
               </div>
 
               <div className='post-title'>
-              <img src={post.subreddit.image_src} />
+               <img src={post.subreddit.image_src} />
                <span className="subreddit-name">r/{post.subreddit.name}</span>
                <span className="post-user">Posted by</span>
                <span className="post-user underline">u/{post.username}</span>
@@ -32,8 +33,27 @@ function Post(){
               </div>
               
               
-              <div className='post-footer'>Footer</div>
+           <div className='post-footer'>
+              <div className="comments footer-action">
+                <ModeComment className="comment-icon" />
+                <span>{post.comments} Comments</span>
+              </div>
+              
+              <div className="share footer-action">
+                <Share />
+                <span>Share</span>
+              </div>
+
+              <div className="save footer-action">
+                <Bookmark />
+                <span>Save</span>
+              </div>
+
+              <MoreHoriz className="more-icon footer-action" />
+          
             </div>
+
+          </div>
            ))}
         </div>
     );
